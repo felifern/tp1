9dcc0ed8f2d0e0784c1776797f788efd79a78686
felipe fernandez luna <ffernandezl@fi.uba.ar>
2025-06-17 05:11:52 +0000

    elementos y operadores, etc

--- main.c
+++ main.c
@@ -4,34 +4,7 @@
 #include "racional.h"
 #include "cola.h"
 #include "lista.h"
-
-
-typedef struct{
-    char *operador;
-    void (*funcion) (void);
-    char *descripcion;
-}operador; //Si agrego void *funcion deberia hacerse algun wrapper para poder englobar todo. (creo)
-/*
-el tema del wrapper es para ver como pasar del void * al puntero a la funcion que necesitamos usar.
-pero no debe haber muchos tipos de funciones para definir. (en realidad no se, imagino que debe haber a los sumo 4 o 6)
-*/
-
-// entonces en la pila apilamos elemntos_t y si son operadores o funciones podemos buscarlos en al lista que nos
-// obligan a hacer. 
-
-// capaz hacer el elemento.h es innecesario, se puede definir todas esas estructuras aca. 
-// queda mas desprolijo supongo, pero es mas rapido(?.
-
-
-
-lista_t lista_crear();
-
-
-
-
-
-
-
+#include "operador.h"
 
 
 cola_t *leer_linea(); // lee la linea y la guarda en una cola. ej: {"123", "+", "2", "*", "5", "+", "fact", "6"} (desactualizado pero es como la idea)
@@ -39,16 +12,66 @@ cola_t *leer_linea(); // lee la linea y la guarda en una cola. ej: {"123", "+", 
 
 cola_t *pasar_a_postfija(cola_t *infija); //
 
-racional_t *operar_postfija(cola_t *polaca /*,racional_t *(*operar)(const racional_t*a, const racional_t *b)*/){
+racional_t *operar_postfija(cola_t *polaca){
     //la idea seria tipo plantear una cola con elementos_t los defino el el .h
     //si la cadena es isdigit, entonces es numero, si es is alpha entonces es funcion, 
-    //si es cada caracter especifico entonces es operador.
-
+    //si es una palabra que sea una funcion entonces es funcion.
+}
+void suma_wrpr (void **a){
+    racional_t *resultado = racional_sumar((const racional_t *)a[0], (const racional_t *)a[1]);
+    a[0] = resultado;
 }
 
 
 
-int main(){
+int main(){    
+    // en el intento del ejemplo me di cuenta que faltan definir una banda de cosas (a lo ultimo digo algo al respecto)
+    // lo del elemento.h esta bien, operador.h me genera dudas,
+    // pero supongo que de alguna forma hay que hacer lo que nos piden
+
+    // imaginamos que de la pila se desapilan "3", "2", "+" o como funcione el algoritmo, queremos sumar 2+3
+    racional_t *a;//ponele que es el 2
+    racional_t *b;// ponele que es el 3
+    racional_t **elementos;// aca guardo el 2 y el 3
+
+    operador_t suma;// asi deberiamos definir las cosas ponele
+    suma.funcion = suma_wrpr;
+    
+    // desapilando los 2 tenemos (esto tenemos que definir, sino no se puede pensar esto) supone un puntero a punteros
+    // racional_t entonces si yo paso eso a la funcion deberia funcionar y devolver por el primero el resultado
+
+    elementos[0] = a; 
+    elementos[1] = b;
+
+    suma.funcion(elementos);
+    //ahora el resultado estaria en elementos[0](es un ejemplo igual, no se si seria bueno implementar asi)
+    racional_imprimir(elementos[0]);
+
+    // realmente esto es una mierda, pero ahora hay algo con lo que preguntar. igual hay que ver que tan lejos esta
+    // de ser una buena idea. 
+
+    // lo que dije que falta definir una "banda" de cosas es mas que nada como vamos a tratar a los elementos que
+    // vayamos apilando y manipulando. pero esto ultimo es mas que nada la logica de la funcion que opera todo esto,
+    // tipo porque llegamos con los char* dependiendo del tipo que sean hacemos distintas cosas. (si son numeros
+    // los pasamos a racional en tiempo de ejecucion y los volvemos a transformar a char*. si son operadores y
+    // los tenemos que usar los buscamos y los usamos como propuse arriba). Y escribiendo esto me da un poco la 
+    // pauta de que manejar esa memoria va a ser un quilombo y se deberian hacer muchos pases de tipos.
+
+    // otra cosa con operador.h como tenemos que saber bien la firma de cada funcion tampoco habria alguna forma de 
+    // agregar operaciones a traves de una funcion como si fuera un TDA. 
+    // esto (el ej) lo hago bajo la asuncion de que deberiamos trabajar con cualquier tipo de funcion que se le pueda
+    // ocurrir al tipo que vaya a usar el programa. 
+    // ponele que todo esto se simplifica a funciones que reciban 0, 1 o 2 (3 a lo sumo) parametros de tipo racional_t y 
+    // devuelvan racional_t ahi seria distinto, se podria hacer todo esto que estoy diciendo mucho mas facil
+    // o eso creo. ademas, porque agregaria una funcion que no opere numeros, pero bueno hay que preguntar.
+
+
+    // es medio choto esto la verdad, y no se cuanto de todo esto se entienda asi hablando por COMENTARIOS DE VSCODE,
+    // tremendo, ademas no se me cae una tilde, tampoco se poneralas con el teclado en ingles srry.
+
+
+
+
 
     printf("pastel de papa");
     printf("pastel de papaAAAAA");
---
+++ operador.h
@@ -0,0 +1,30 @@
+#ifndef OPERADOR_H
+#define OPERADOR_H
+// lamentablemente llegue a la conclusion de que esto es necesario
+
+typedef struct{
+    char *operador;
+    void (*funcion) (void **argumentos);
+    int aridad;
+    int prioridad;
+    char *descripcion;
+}operador_t;
+// mmmmmmmmmmm. la idea seria pasar argumentos con void** cada funcion particular sabe que argumentos usar.
+// dejo un ejemplo en el main.c 
+
+
+//comentario anterior:
+//Si agrego void *funcion deberia hacerse algun wrapper para poder englobar todo. (creo)
+/*
+el tema del wrapper es para ver como pasar del void * al puntero a la funcion que necesitamos usar.
+pero no debe haber muchos tipos de funciones para definir. (en realidad no se, imagino que debe haber a los sumo 4 o 6)
+*/
+
+// entonces en la pila apilamos elemntos_t y si son operadores o funciones podemos buscarlos en al lista que nos
+// obligan a hacer. 
+
+// capaz hacer el elemento.h es innecesario, se puede definir todas esas estructuras aca. 
+// queda mas desprolijo supongo, pero es mas rapido(?.
+
+
+#endif
